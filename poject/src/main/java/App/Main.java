046f168968039e214ca5f541845fafb95c7f50fc
herbakmarcell <herbakmarcell@gmail.com>
2024-05-11 12:40:41 +0200

    basic components

---
+++ poject/src/main/java/App/Main.java
@@ -0,0 +1,11 @@
+package App;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+    private static Logger logger = Logger.getLogger("App.Main logger");
+    public static void main(String[] args) {
+        StartPage form = new StartPage();
+        logger.info("Start page was shown");
+    }
+}
