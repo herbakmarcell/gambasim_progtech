45d8672a609598c5192fdd848a401d28903ea860
herbakmarcell <herbakmarcell@gmail.com>
2024-05-12 17:03:44 +0200

    LoginPage done

---
+++ poject/src/main/java/SelectGame/SelectGamePage.java
@@ -0,0 +1,4 @@
+package SelectGame;
+
+public class SelectGamePage {
+}
--- poject/src/main/java/Start/StartPage.java
+++ poject/src/main/java/Start/StartPage.java
@@ -1,4 +1,70 @@
 package Start;
 
+import App.UserData;
+import Database.DatabaseConnection;
+import Database.LoginCommand;
+import SelectGame.SelectGamePage;
+import org.apache.log4j.Logger;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowEvent;
+
 public class StartPage {
+    private static Logger logger = Logger.getLogger("Login logger");
+    private DatabaseConnection db = new DatabaseConnection(
+            "jdbc:mysql://localhost:3306/gambasim",
+            "root",
+            "");
+    private final JFrame frame;
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JPanel mainPanel;
+    private JLabel pageLabel;
+    private JLabel usernameLabel;
+    private JLabel passwordLabel;
+    private JButton loginButton;
+
+    public StartPage(){
+        frame = CreateFrame();
+        ConfigureJFrame(frame);
+        RegisterListeners();
+    }
+    private JFrame CreateFrame() {
+        logger.info("Created login frame");
+        return new JFrame();
+    }
+
+    private void ConfigureJFrame(JFrame frame){
+        frame.setContentPane(this.mainPanel);
+        frame.pack();
+        frame.setSize(300,300);
+        frame.setTitle("Gamba Szimulátor - Bejelentkezés");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        logger.info("Login frame configured");
+    }
+
+    private void RegisterListeners() {
+        loginButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (TryLogin()){
+                    SelectGamePage selectPage = new SelectGamePage();
+                    frame.setVisible(false);
+                    frame.dispose();
+                }
+            }
+        });
+    }
+    private boolean TryLogin(){
+        String username = usernameField.getText();
+        String password = String.valueOf(passwordField.getPassword());
+        LoginCommand login = new LoginCommand(db, username, password);
+        login.execute();
+        if (UserData.username == null) { return false; }
+        else { return true; }
+    }
 }
